a5525a4887a0a37df557fd713dc0dc507ffd3e9d
cwiggin9 <cwiggins.g@gmail.com>
2022-10-10 19:53:46 -0500

    created game logic to check for win

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const gameBoard = (() => {
-    const gameboard = [
-        ['X', 'X', 'X'],
-        ['O', 'O', 'O'],
-        ['X', 'O', 'X']
+    let gameboard = [
+        ['A', 'AA', 'AAA'],
+        ['B', 'BB', 'BBB'],
+        ['C', 'CC', 'CCC']
     ];
     return {
         gameboard
@@ -21,36 +21,54 @@ const displayController = (() => {
 
     AddMark = (event) => {
         if(event.currentTarget.textContent === ""){
-            event.currentTarget.textContent = currentPlayer.mark;
             currentPlayer = currentPlayer === X ? O : X
-            RenderContents();
+
+            RenderContents(event, currentPlayer.mark);
+
+            if(WinCheck() == true) RemoveListeners();
         };
     }
-
     return { AddMark }
 })();
 
-const gameboard = document.getElementById("gameboard");
-
+// Adds 'click' listeners to grid elements
 const squares = document.querySelectorAll(".square");
 squares.forEach(square => {
     square.addEventListener("click", displayController.AddMark);
 });
 
-function RenderContents() {
-    const boardRef = gameBoard.gameboard;
-    let counter = 0;
-
-    for(let i = 0; i < boardRef.length; i++){
-        for(let j = 0; j < boardRef.length; j++){
-            boardRef[i][j] = squares[counter].textContent;
-            counter++;
-        }
-    }
-};
-
+// A function that removes the 'click' listeners from the grid elements
 function RemoveListeners(){
     squares.forEach(square => {
         square.removeEventListener("click", displayController.AddMark);
     });
+}
+
+function RenderContents(event, mark){
+    event.currentTarget.textContent = mark;
+    let x = event.target.getAttribute('data-x');
+    let y = event.target.getAttribute('data-y');
+    gameBoard.gameboard[x][y] = mark;
+}
+
+// A function that checks for a winning match on the board
+function WinCheck(){
+    let boardRef = gameBoard.gameboard;
+    let win = false;
+
+    for(let i = 0; i < 3; i++){
+        // horizontal match check
+        if((boardRef[i][0] === boardRef[i][1]) && (boardRef[i][0] === boardRef[i][2])){
+            win = true;
+        }
+
+        // vertical match check
+        if((boardRef[0][i] === boardRef[1][i]) && (boardRef[0][i] === boardRef[2][i])){
+            win = true;
+        }
+    }
+    // diagonal match checks
+    if((boardRef[0][0] === boardRef[1][1]) && (boardRef[0][0] === boardRef[2][2])) win = true;
+    if((boardRef[0][2] === boardRef[1][1]) && (boardRef[0][2] === boardRef[2][0])) win = true;
+    return win;
 }
